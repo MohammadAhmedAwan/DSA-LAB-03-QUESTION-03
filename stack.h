6ab0ffcb191ec306c49488ba701de440fd758d8e
MohammadAhmedAwan <156419437+MohammadAhmedAwan@users.noreply.github.com>
2024-10-01 18:30:30 +0500

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "node.h"
+
+
+class stack {
+public:
+
+    stack();
+
+
+    node* head;
+};
+
+
+node* concatenate(node* head1, node* head2);
+
+
+node* findIntersection(node* head1, node* head2);
+
+
+int compareStrings(node* head1, node* head2);
